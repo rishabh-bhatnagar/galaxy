d727595e609e85961432a475c9f8d3965fe20c35
rishabh-bhatnagar <bhatnagarrishabh4@gmail.com>
2018-12-22 14:00:33 +0530

    PeP8  convention

--- folder_duplicate.py
+++ folder_duplicate.py
@@ -1,12 +1,13 @@
 import pandas as pd
-import csv
+from xlsxwriter.workbook import Workbook
 
 orig_sheet_location = "execution-result.log.csv"
 sheet_location = 'temp.csv'
 output_csv_location = 'output.csv'
-var_all_counts ='all_opf_count'
+var_all_counts = 'all_opf_count'
 var_all_folder_name = 'folder_links_list'
 var_opf_links = 'all_opf_links'
+
 
 def replace_all_commas():
     import codecs
@@ -19,7 +20,6 @@ def replace_all_commas():
             lines.append(line)
     keys = []
     values = []
-    rows = []
     written = False
 
     for myOrderedDict in lines:
@@ -29,16 +29,16 @@ def replace_all_commas():
             value = value.replace(',', ':,:')
             values.append(value)
 
-        with open(sheet_location, "w") as outfile:
-            csvwriter = csv.writer(outfile, lineterminator='\n')
+        with open(sheet_location, "w"):
             if not written:
                 written = True
 
-    values = [values[i:i+3] for i in range(0, len(values), 3)]
+    values = [values[i:i + 3] for i in range(0, len(values), 3)]
     with open(sheet_location, "w") as outfile:
-            csvwriter = csv.writer(outfile, lineterminator='\n')
-            csvwriter.writerow(keys)
-            csvwriter.writerows(values)
+        csvwriter = csv.writer(outfile, lineterminator='\n')
+        csvwriter.writerow(keys)
+        csvwriter.writerows(values)
+
 
 def list_like_to_list(string):
     string = string.replace(':,:', ',')
@@ -48,13 +48,14 @@ def list_like_to_list(string):
     for i in string.split(","):
         yield i.strip()
 
+
 replace_all_commas()
 
 df = pd.read_csv(sheet_location)
 df.columns = df.iloc[0]
 df = df.T
 df.columns = df.iloc[0]
-df.apply(lambda x: x.str.replace(':,:',','))
+df.apply(lambda x: x.str.replace(':,:', ','))
 
 all_counts = [int(i) for i in list_like_to_list(df[var_all_counts][2])]
 all_folder_names = list(list_like_to_list(df[var_all_folder_name][2]))
@@ -72,8 +73,8 @@ for i in range(len(all_counts)):
         for j in range(count_opf):
             to_csv.append([folder_name, all_opf_links[opf_link_count]])
             opf_link_count += 1
-to_csv += ['folder name', 'opf link']
-from xlsxwriter.workbook import Workbook
+to_csv = [['folder name', 'opf link']] + to_csv
+
 workbook = Workbook('output.xlsx')
 worksheet = workbook.add_worksheet()
 for i, row in enumerate(to_csv):
