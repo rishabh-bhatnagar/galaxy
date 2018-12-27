18238ff32fa565247dc060bee35003e867fab765
rishabh-bhatnagar <bhatnagarrishabh4@gmail.com>
2018-12-27 09:34:44 +0530

    wrote to xlsx via dataframe of pandas

--- XML/xml_another_try.py
+++ XML/xml_another_try.py
@@ -1,12 +1,17 @@
-import xml.etree.ElementTree
-import re
-from itertools import groupby
 import csv
+import re
+import xml.etree.ElementTree
+from itertools import groupby
 from os import listdir
+from pandas import DataFrame
 
 def get_node_value(e, l):
     return eval('e.getroot(){}.text'.format(''.join(['[{}]'.format(i) for i in l])))
+
+
 res = []
+
+
 def recursive_iterate(root, history, init):
     if init:
         global res
@@ -18,6 +23,8 @@ def recursive_iterate(root, history, init):
         for i in range(len(a)):
             recursive_iterate(root[i], '{}, {}'.format(history, i), False)
     return res
+
+
 def merge_lists(lt):
     if len(lt) == 1:
         return lt
@@ -25,6 +32,8 @@ def merge_lists(lt):
     a[0][-2] = sum([i[0][-2] for i in lt]) / len(lt)
     a[1] = ' '.join([i[1] for i in lt])
     return a
+
+
 def re_replace(replacee, replacer, string):
     import re
     insensitive_hippo = re.compile(re.escape(replacee), re.IGNORECASE)
@@ -58,10 +67,12 @@ def parse_address_table(res1212):
         address_table = create_table(table21)
         address_table = list(zip(*address_table))
         return address_table
+
     def hard_code_field_find(block, field):
         for i, b in enumerate(block):
             if field.lower() in b.lower():
                 return i
+
     def without_state_parse(block, dict_prefix):
         block = list(block)
         which_address = block.pop(0)
@@ -85,8 +96,10 @@ def parse_address_table(res1212):
             tel = re_replace('tel', '', block[state_idx + 2]).replace('#', '')
             email = re_replace('email', '', block[state_idx + 3]).strip("#").strip(":-")
             gstn, pan = block[state_idx + 4].split(', ')
-            gstn = re_replace('gst', '', gstn).strip().strip('N').strip('n').strip('NO').strip('no').strip().strip(":").strip().replace('NO', '').replace(':', '')
-            pan = re_replace('pan', '', pan).strip().strip('N').strip('n').strip('NO').strip('no').strip().strip(":").strip().replace('NO', '').replace(':', '').strip('=-')
+            gstn = re_replace('gst', '', gstn).strip().strip('N').strip('n').strip('NO').strip('no').strip().strip(
+                ":").strip().replace('NO', '').replace(':', '')
+            pan = re_replace('pan', '', pan).strip().strip('N').strip('n').strip('NO').strip('no').strip().strip(
+                ":").strip().replace('NO', '').replace(':', '').strip('=-')
             f1212 = {
                 dict_prefix + 'address': address,
                 dict_prefix + 'state': state,
@@ -106,14 +119,19 @@ def parse_address_table(res1212):
         delivery_address="\n".join(address_table[1][1:])
     )
 
+
 def get_index_by_substr(block, substr):
     for i in block:
         if substr in " ".join([u for u in i[1].split(' ') if u]).lower():
             return block.index(i)
+
+
 def print_table(table_data):
     widths = [max(map(len, col)) for col in zip(*table_data)]
     for row in table_data:
         print("  ".join((val.ljust(width) for val, width in zip(row, widths))))
+
+
 def create_table(elements):
     row_min = min(elements, key=lambda x: x[0][0])[0][0]
     col_min = min(elements, key=lambda x: x[0][1])[0][1]
@@ -126,6 +144,8 @@ def create_table(elements):
     # table = [[str(i) for i in range(col_max)]] + table
     # print_table(table)
     return table
+
+
 def parse_sales_table(res):
     def get_sales_table(res):
         i1 = get_index_by_substr(res, 'sales detail') + 1
@@ -170,22 +190,30 @@ def parse_sales_table(res):
     else:
         result_dict['grand total'] = remaining_table[5][4]
     return result_dict
+
+
 def get_field(all_fields, identifier, split_by):
     for field in all_fields:
         if identifier in ' '.join([i for i in field[1].split(' ') if i]).lower():
             return field[1].split(split_by)[-1].strip()
+
+
 def get_max_difference_idx(row):
     diffs = []
-    row = sorted(row, key=lambda x:x[0][1])
-    for i in range(len(row)-1):
-        diffs.append(row[i+1][0][1]-row[i][0][1])
+    row = sorted(row, key=lambda x: x[0][1])
+    for i in range(len(row) - 1):
+        diffs.append(row[i + 1][0][1] - row[i][0][1])
     return diffs.index(max(diffs))
+
+
 def get_closest(block, field, split_by):
     for i, ele in enumerate(block):
         if field in " ".join([i for i in ele.lower().split() if i]):
             block.pop(i)
             return block, ele.split(split_by)[-1]
     return block, None
+
+
 def get_loose_data(res):
     for i, ele in enumerate(res):
         if len(ele[0]) == 3:
@@ -194,7 +222,7 @@ def get_loose_data(res):
     a = res[i]
     while len(a[0]) == 3:
         block.append(a)
-        a = res.pop(i+1)
+        a = res.pop(i + 1)
     block = [i for i in block if i[1]]
     pairs = []
     for k, v in groupby(block, key=lambda x: x[0][0]):
@@ -202,16 +230,16 @@ def get_loose_data(res):
         for i in v:
             group.append([i[0][:-1], i[1]])
         group_0_1 = sorted([i[0][1] for i in group])
-        for i in range(max(group, key=lambda x:x[0][1])[0][1]):
+        for i in range(max(group, key=lambda x: x[0][1])[0][1]):
             if i not in group_0_1:
                 group.append([[group[0][0][0], i], ''])
-        group = sorted(group, key=lambda x:x[0][1])
+        group = sorted(group, key=lambda x: x[0][1])
         string = " ".join([i[1] for i in group])
         n_spaces = 1
-        while " "*n_spaces in string:
+        while " " * n_spaces in string:
             n_spaces += 1
         n_spaces = max(2, n_spaces)
-        pairs.extend(string.split(" "*(n_spaces-1)))
+        pairs.extend(string.split(" " * (n_spaces - 1)))
 
     pairs, sales_person = get_closest(pairs, 'sales person', ':')
     pairs, opf_no = get_closest(pairs, 'opf no', '.')
@@ -219,14 +247,18 @@ def get_loose_data(res):
     pairs, date = get_closest(pairs, 'date', ':')
     pairs, purch_order_no = get_closest(pairs, 'order no', ':')
     pairs, pot_id = get_closest(pairs, 'pot id', ':')
-    return dict(sales_person=sales_person, opf_no=opf_no, customer_name=customer_name, date=date, purch_order_no=purch_order_no, pot_id=pot_id)
+    return dict(sales_person=sales_person, opf_no=opf_no, customer_name=customer_name, date=date,
+                purch_order_no=purch_order_no, pot_id=pot_id)
+
+
 def write_to_csv(file_name):
     e = xml.etree.ElementTree.parse(file_name)
     wt_elements = []
     for element in e.iter():
         if element.tag.split('}')[-1] == 't':
             wt_elements.append(element)
-    res = [[i[0][4:], i[1].strip()] for i in [[i, get_node_value(e, i)] for i in sorted([[int(i) for i in string[1:].split(', ')] for string in recursive_iterate(e.getroot(), '', True)])] if i[1]]
+    res = [[i[0][4:], i[1].strip()] for i in [[i, get_node_value(e, i)] for i in sorted(
+        [[int(i) for i in string[1:].split(', ')] for string in recursive_iterate(e.getroot(), '', True)])] if i[1]]
     loose_data = get_loose_data(res.copy())
 
     res = [i for i in res if i[1]]
@@ -236,31 +268,19 @@ def write_to_csv(file_name):
     all_details = loose_data.copy()
     all_details.update(address_details)
     all_details.update(sales_details.copy())
-    all_details[file_name]=file_name
-    header_present = False
-    try:
-        with open('output.csv', 'r') as csv_file:
-            reader = csv.reader(csv_file)
-            for data in reader:
-                if len(data)>1:
-                    header_present = True
-                    break
-    except FileNotFoundError:
-        header_present = False
+    return all_details
 
-    with open('output.csv', 'a', newline='') as csv_file:
-        writer = csv.DictWriter(csv_file, fieldnames=all_details.keys())
-        if not header_present:
-            writer.writeheader()
-        writer.writerow(all_details)
-write_to_csv('OPF- TK-024.xml')
-'''
+
 import traceback
+dicts = []
 for file_name in listdir():
     try:
-        write_to_csv(file_name)
+        result_dict = write_to_csv(file_name)
+        dicts.append(result_dict)
     except Exception as e:
         print('error in', file_name)
         traceback.print_exc()
         print('\n\n\n')
-'''
+result = DataFrame(dicts)
+print(result)
+result.to_excel('output.xlsx')
