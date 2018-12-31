1852db4ec710c26f09325fbb02174c80ce66cbb5
rishabh-bhatnagar <bhatnagarrishabh4@gmail.com>
2018-12-31 15:01:39 +0530

    desc, qty, unit price restructured df

--- pwd/doc_to_xml.py
+++ pwd/doc_to_xml.py
@@ -2,7 +2,6 @@ from docx import Document
 import shutil
 import os.path
 from pandas import DataFrame as DF
-from glob import glob
 import re
 import os
 from os import listdir
@@ -10,8 +9,12 @@ from os.path import abspath
 import win32com.client as win32
 from win32com.client import constants
 
-# Create list of paths to .doc files
-paths = glob('.\\*.doc')
+state_mapping = dict({
+    "bangalore": "Karnataka",
+    'tamil nadu': 'tamilnadu',
+    'hyderabad': 'andhra pradesh'
+})
+state_mapping.setdefault(None, '')
 
 
 class OPF():
@@ -79,12 +82,22 @@ class OPF():
         tables = self.get_tables()
         address_table = self.create_table(tables[0])
         sales_table = self.create_table(tables[1])
+
         address_data = self.parse_address_tables(address_table)
         sales_data = self.parse_sales_data(sales_table)
         loose_fields = self.get_loose_fields()
+
         final_result = address_data.copy()
         final_result.update(sales_data)
         final_result.update(loose_fields)
+        state = final_result.get('badstate')
+        state = state if state is not None else ''
+        if state:
+            state = state.lower()
+        final_result.update(dict(state_to_dc_state=state_mapping.get(state)))
+        if not final_result.get('state_to_dc_state'):
+            final_result['state_to_dc_state'] = state.capitalize()
+
         return final_result
 
     def get_element_from_block(self, block: list, identifier: str, split_by: str) -> str:
@@ -116,9 +129,6 @@ class OPF():
             if first_field_index is None:
                 return {}  # an empty dict suggesting failure in parsing all fields.
             name = block[0]
-            for i in block:
-                print(i)
-            print('-'*300)
             address = "\n".join(block[:first_field_index])
             block = block[first_field_index:]
             state = self.get_element_from_block(block, 'State', ':')
@@ -131,6 +141,10 @@ class OPF():
                     state = 'Maharashtra'
                     # print('setting state as Maharashtra')
                 # print('state not found')
+            if state:
+                print(state)
+                res_state = state_mapping.get(state.lower())
+                state = res_state if res_state else state
             contact_person = self.get_element_from_block(block, 'Contact Person', ':')
             email = self.get_element_from_block(block, 'Email', '#')
             tel = self.get_element_from_block(block, 'tel', '#')
@@ -159,10 +173,6 @@ class OPF():
             )
 
         address_table = [list(i) for i in zip(*address_table)]
-        print('@'*300)
-        for i in address_table:
-            print(i)
-        print('@' * 300)
         billing_address = {'bad' + k: v for k, v in parse_address_block(address_table[0]).items()}
         delivery_address = {'dad' + k: v for k, v in parse_address_block(address_table[1]).items()}
         billing_address.update(delivery_address)
@@ -171,7 +181,9 @@ class OPF():
     def parse_sales_data(self, sales_table):
         header = sales_table.pop(0)
         result = {}
+        i = 0
         while True:
+            i += 1
             sr_no = sales_table[0][0]
             if not [i for i in sr_no if i.isdigit()]: break
             result.update({'desc_' + sr_no: sales_table[0][1]})
@@ -179,6 +191,9 @@ class OPF():
             result.update({'unit_price_' + sr_no: sales_table[0][3]})
             result.update({'total_price_' + sr_no: sales_table[0][1]})
             sales_table.pop(0)
+        i -= 1
+        i = max([i, 1])
+        result.update(dict(number_of_products=i))
         for i in sales_table:
             for j in i:
                 if 'CGST' in j:
@@ -249,7 +264,7 @@ if __name__ == '__main__':
             OPF(abspath(path)).seperate_doc()
     os.chdir(abspath(docx_folder_name))
     result_dict_list = []
-    # opf = OPF('OPF 169 - bluezone - LT.DT.docx').extract_data()
+    opf = OPF('OPF- TK-024.docx').extract_data()
     for i, file_name in enumerate(listdir()):
         if '.docx' in file_name:
             print(file_name)
@@ -257,27 +272,33 @@ if __name__ == '__main__':
             data_dict = opf.extract_data()
             data_dict.update({'opf link': file_name.split('.')[0]})
             result_dict_list.append(data_dict)
-    # result_dict_list = [i for i in result_dict_list if i['badstate']]
+    result_dict_list = [i for i in result_dict_list if i['badstate']]
     # result_dict_list = sorted(result_dict_list, key=lambda x:x['badstate'] if x['badstate'] is not None else '')
     df = DF(result_dict_list)
     all_keys = list(df.keys())
-
-
-    df = df[
-        [
-            all_keys.pop(all_keys.index('dadstate')),
-            all_keys.pop(all_keys.index('opf_no')),
-            all_keys.pop(all_keys.index('customer_name')),
-            all_keys.pop(all_keys.index('purch_order_no')),
-            all_keys.pop(all_keys.index('opf_date')),
-            all_keys.pop(all_keys.index('payment_terms')),
-            all_keys.pop(all_keys.index('dadname')),
-            all_keys.pop(all_keys.index('dadaddress')),
-            all_keys.pop(all_keys.index('dadgstn')),
-
-            all_keys.pop(all_keys.index('opf link')),
-            all_keys.pop(all_keys.index('opf_location')),
-            all_keys.pop(all_keys.index('pot_id'))
-        ]
-    ]
+    header = [
+                 all_keys.pop(all_keys.index('state_to_dc_state')),
+                 all_keys.pop(all_keys.index('dadstate')),
+                 all_keys.pop(all_keys.index('opf_no')),
+                 all_keys.pop(all_keys.index('customer_name')),
+                 all_keys.pop(all_keys.index('purch_order_no')),
+                 all_keys.pop(all_keys.index('opf_date')),
+                 all_keys.pop(all_keys.index('payment_terms')),
+                 all_keys.pop(all_keys.index('dadname')),
+                 all_keys.pop(all_keys.index('dadaddress')),
+                 all_keys.pop(all_keys.index('dadgstn')),
+                 all_keys.pop(all_keys.index('badname')),
+                 all_keys.pop(all_keys.index('badaddress')),
+                 all_keys.pop(all_keys.index('badgstn')),
+                 all_keys.pop(all_keys.index('number_of_products')),
+             ]
+    desc = sorted([i for i in all_keys if 'desc' in i], key=lambda x: int("".join([i for i in x[5:] if i.isdigit()])))
+    qty = sorted([i for i in all_keys if 'qty' in i], key=lambda x: int("".join([i for i in x[4:] if i.isdigit()])))
+    unit_price = sorted([i for i in all_keys if 'unit_price' in i], key=lambda x: int("".join([i for i in x[11:] if i.isdigit()])))
+    all_keys = [i for i in all_keys if i not in desc+unit_price+qty]
+    rest_fields = all_keys.pop()
+    unpacked_tuples = []
+    for i in zip(desc, qty, unit_price):
+        unpacked_tuples += i
+    df = df[header+unpacked_tuples]
     df.to_excel('../final_output.xlsx')  # saving out of docx folder.
