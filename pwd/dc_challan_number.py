e6c946864eb66e38ed010b3465a01f3f6adc5fcb
rishabh-bhatnagar <bhatnagarrishabh4@gmail.com>
2019-01-04 15:38:28 +0530

    added dict of data in program itself.

--- pwd/dc_challan_number.py
+++ pwd/dc_challan_number.py
@@ -1,14 +1,30 @@
 from csv import DictWriter, DictReader
 from sys import argv
+current_year = int(str(__import__('datetime').datetime.now().year)[-2:])
 
 
 if len(argv) < 2:
-    print("Cannot process request. Cannot ")
+    print("Cannot process request. Expected exactly 1 field.")
+    exit(0)
+state = argv[1].lower()
+
 file_name = 'challan_number.csv'
-data = []
-with open(file_name, 'r') as file:
-    reader = DictReader(file)
-    data = (i for i in reader)
+
+og_data = [
+    dict(state='state', abbrevation='abbrevation', last_no='last_no'),
+
+    dict(state='telangana', abbrevation='hy', last_no='2'),
+dict(state='maharashtra', abbrevation='mh', last_no='1'),
+    dict(state='chennai', abbrevation='mh', last_no='1')
+]
+
 
 with open(file_name, 'w') as file:
-    wri = 345
+    writer = DictWriter(file, fieldnames=og_data[0].keys())
+    writer.writerows(og_data)
+with open(file_name, 'r') as file:
+    reader = DictReader(file)
+    data = [dict(i) for i in reader]
+
+index_state = [i['state'] for i in data].index(state)
+print(data[index_state])
